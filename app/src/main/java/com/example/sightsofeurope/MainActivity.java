e566cbc8eb8d7c136ca5f837649cd8dcc92874eb
ALupic <alexlu_1995@hotmail.com>
2019-11-18 22:34:31 +0100

    Initial commit

---
+++ app/src/main/java/com/example/sightsofeurope/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.sightsofeurope;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView firstTextView = (TextView) findViewById(R.id.firstTextView);
+        Typeface tfFirstTextView = Typeface.createFromAsset(getAssets(), "fonts/Orienta-Regular.ttf");
+        firstTextView.setTypeface(tfFirstTextView);
+
+        Button startButton = (Button) findViewById(R.id.startButton);
+        Typeface tfStartButton = Typeface.createFromAsset(getAssets(), "fonts/Orienta-Regular.ttf");
+        startButton.setTypeface(tfStartButton);
+        startButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                Intent startIntent = new Intent(getApplicationContext(), CountryListActivity.class);
+                startActivity(startIntent);
+            }
+        });
+    }
+}
